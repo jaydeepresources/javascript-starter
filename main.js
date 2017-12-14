28412cb81db24daebf449e9b44f648c4b90058e9
jaydeepresources <jaydeepresources@github.com>
2017-12-14 17:52:34 +0530

    starter

--- main.js
+++ main.js
@@ -12,6 +12,55 @@ function showDate() {
 function hideDate() {
     document.getElementById('date').innerHTML = "";
 }
-function print(){
-    console.log('hi');
+
+var flag = true;
+
+function toggleSection() {
+    var someText = document.getElementById('someText');
+    var caption = document.getElementById('toggle');
+
+    flag = !flag;
+
+    if (!flag) {
+        someText.style.display = 'none';
+        caption.innerHTML = 'show';
+        caption.className = 'btn btn-success';
+    }
+    else {
+        someText.style.display = 'block';
+        caption.innerHTML = 'hide';
+        caption.className = 'btn btn-danger';
+    }
+}
+
+var hobbies = ['music', 'films', 'dance', 'books'];
+
+function iterateHobbies() {
+    var list = "";
+    for (var i = 0; i < hobbies.length; i++) {
+        list += "<a class='list-group-item list-group-item-action'>"
+            + hobbies[i] +
+            "<span><button type='button' onclick='deleteRow(" + i + ")' class='close' data-dismiss='alert'>&times;</button></span>"
+        "</a>";
+    }
+    document.getElementById('hobbiesList').innerHTML = list;
+}
+
+function deleteRow(i) {
+    hobbies.splice(i, 1);
+    iterateHobbies();
+}
+
+function tabs() {
+    var emp = [[1, 'john'], [2, 'adam'], [3, 'harris'], [4, 'luke']];
+    var data = document.getElementById('data');
+    var tableEmp = "<table class='table table-hover'><tr><th>Id</th><th>Name</th></tr>";
+    for (var i = 0; i < emp.length; i++) {
+        tableEmp += "<tr>";
+        for (var j = 0; j < emp[i].length; j++) {
+            tableEmp += "<td>" + emp[i][j] + "</td>";
+        }
+        tableEmp += "</tr>";       
+    }
+    data.innerHTML = tableEmp;
 }
